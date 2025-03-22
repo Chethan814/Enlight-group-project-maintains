93cfa144a9e09cb97a8966bdbba973e69b033438
chethan <chethanchethu99339@gmail.com>
2025-03-22 07:37:23 +0530

    Add new Nursing page with corresponding views

---
+++ resources/WebView/Nursing/MSCNursing.blade.php
@@ -0,0 +1,219 @@
+@extends('WebView.WebViewLayouts.WebViewLayout')
+
+@section('webContent')
+    <!-- breadcrumbarea__section__start -->
+
+    <div class="breadcrumbarea" style="background-image: url('{{asset('assets/img/infobanner/infobanner-6.jpg')}}')">
+        <div class="dark-overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="breadcrumb__content__wraper" data-aos="fade-up">
+                        <div class="breadcrumb__title">
+                            <h2 class="heading text-white">M.Sc. Nursing</h2>
+                        </div>
+                        <div class="breadcrumb__inner">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li>M.Sc. Nursing</li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- breadcrumbarea__section__end-->
+
+    <!-- bsc-nursing__start -->
+    <div class="blogarea__2 sp_top_100 sp_bottom_100">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xl-8 col-lg-8">
+
+                    <div class="blogarae__img__2 course__details__img__2" data-aos="fade-up">
+                        <img loading="lazy" src="{{ asset('assets/img/courses/course-2.jpg') }}" alt="blog">
+                    </div>
+
+                    <div class="blog__details__content__wraper">
+                        <div class="course__button__wraper" data-aos="fade-up">
+                            <div class="course__button">
+                                <a href="javascript:void(0);"><i class="icofont-file-document"></i> Download
+                                    Prospectus</a>
+                            </div>
+                            <div class="course__date">
+                                <p>Last Update:<span> May 19, 2024</span></p>
+                            </div>
+                        </div>
+                        <div class="course__details__heading" data-aos="fade-up">
+                            <h3>About this course</h3>
+                        </div>
+                        <div class="course__details__paragraph text-justify" data-aos="fade-up">
+                            <p>MSc Nursing will prepare Nurses, to improve the quality of nursing education and practice, in India. It will build upon and extend, competence acquired at the graduate level, emphasize application of relevant theories into nursing practice, education, administration and development of research skills. It will prepare nurses, for leadership position in nursing and health care fields, who can function as nurse specialists, consultants, educators, administrators and researchers in a wide variety of professional settings in meeting the National priorities and the changing needs of the society.
+                            </p>
+                        </div>
+
+                        <h4 class="sidebar__title" data-aos="fade-up">Eligibility</h4>
+                        <p class="text-justify">The Minimum education requirement shall be the passing of a B.Sc. Nursing/Post Basic B.Sc. Nursing with a minimum of 55% aggregate marks.
+                            <br>
+                            Minimum one year of work experience after Basic B.Sc. Nursing. Minimum one year of work experience before or after Post Basic B.Sc. Nursing
+                        </p>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="course__details__wraper" data-aos="fade-up">
+
+                                    <div class="d-flex align-items-center gap-3">
+                                        <div class="icon-badge">
+                                            <i class="icofont-check-circled"></i>
+                                        </div>
+                                        <div>
+                                            <h5 class="text-primary mb-0">Approvals</h5>
+                                            <p class="lh-1-5 mb-0">Indian Nursing Council ( INC ), Karnataka Nursing Council
+                                                ( KNC )</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <div class="course__details__wraper" data-aos="fade-up">
+
+                                    <div class="d-flex align-items-center gap-3">
+                                        <div class="icon-badge">
+                                            <i class="icofont-clock-time"></i>
+                                        </div>
+                                        <div>
+                                            <h5 class="text-primary mb-0">Duration</h5>
+                                            <p class="mb-0">2 Years</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-12">
+                                <div class="mb-3" data-aos="fade-up">
+                                    <a class="default__button me-2" target="_blank"
+                                        href="https://enlight.eduwego.in/online-admission/enlight-group-of-institutions">Apply
+                                        Now</a>
+                                    <a class="default__button" target="_blank"
+                                        href="https://enlight.eduwego.in/student-enquiry/enlight-group-of-institutions">Enquiry
+                                        Now</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="course__list__wraper" data-aos="fade-up">
+                            <div class="blog__details__heading__2">
+                                <h5>Documents Required For Admission</h5>
+                            </div>
+                            <div class="aboutarea__list__2 blog__details__list__2" data-aos="fade-up">
+                                <ul>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>10th / 12th Marksheet</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>10th / 12th Admit Card</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>Aadhhar Copy</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>Migration</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>12 Pic Colour Passport size Photo</p>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+                <div class="col-xl-4 col-lg-4">
+
+                    <div class="course__details__sidebar">
+
+                        <div class="blogsidebar__content__wraper__2" data-aos="fade-up">
+
+                            <h4 class="sidebar__title">Other Courses</h4>
+                            <ul class="course__details__populer__list">
+                                <li>
+
+                                    <div class="course__details__populer__img">
+                                        <img loading="lazy" src="{{ asset('assets/img/courses/course-1.jpg') }}"
+                                            alt="courses" width="90" height="70">
+                                    </div>
+                                    <div class="course__details__populer__content">
+                                        <h6>
+                                            <a href="/bScNursing">B.Sc. Nursing</a>
+                                        </h6>
+                                    </div>
+                                </li>
+                                <li>
+
+                                    <div class="course__details__populer__img">
+                                        <img loading="lazy" src="{{ asset('assets/img/courses/course-5.jpg') }}"
+                                            alt="courses" width="90" height="70">
+                                    </div>
+                                    <div class="course__details__populer__content">
+                                        <h6>
+                                            <a href="/PbbscNursing">PBBSc Nursing</a>
+                                        </h6>
+                                    </div>
+                                </li>
+                                <li>
+
+                                    <div class="course__details__populer__img">
+                                        <img loading="lazy" src="{{ asset('assets/img/courses/course-6.jpg') }}"
+                                            alt="courses" width="90" height="70">
+                                    </div>
+                                    <div class="course__details__populer__content">
+                                        <h6>
+                                            <a href="/GnmNursing">GNM Nursing</a>
+                                        </h6>
+                                    </div>
+                                </li>
+                            </ul>
+
+                            <div class="course__summery__button">
+                                <p>Need any help in Admission ?</p>
+                                <a class="default__button default__button--3" href="tel:+91 9916073632"><i
+                                        class="icofont-phone"></i> +91 9916073632</a>
+                            </div>
+
+                        </div>
+                        <div class="blogsidebar__content__wraper__2" data-aos="fade-up">
+
+                            <h4 class="sidebar__title">Get in Touch</h4>
+                            <div class="get__touch__input">
+                                <input type="text" placeholder="Enter name*">
+                                <input type="text" placeholder="Enter your mail*">
+                                <input type="text" placeholder="Massage*">
+                                <a class="default__button" href="blog-details.html">Send Message</a>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bsc-nursing__end -->
+@endsection
