3bf9a83b31280863b2e69baf3280a552ca2ddb72
chethan <chethanchethu99339@gmail.com>
2025-03-22 07:37:36 +0530

    Add new Paramedicals page with corresponding views

---
+++ resources/WebView/Paramedicals/Paramedicals.blade.php
@@ -0,0 +1,222 @@
+@extends('WebView.WebViewLayouts.WebViewLayout')
+
+@section('webContent')
+
+<!-- breadcrumbarea__section__start -->
+
+<div class="breadcrumbarea" style="background-image: url('{{asset('assets/img/infobanner/infobanner-6.jpg')}}')">
+    <div class="dark-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="breadcrumb__content__wraper" data-aos="fade-up">
+                    <div class="breadcrumb__title">
+                        <h2 class="heading text-white">Paramedical</h2>
+                    </div>
+                    <div class="breadcrumb__inner">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li>Paramedical</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- breadcrumbarea__section__end-->
+
+<!-- .contact__section__start -->
+<div class="gridarea">
+    <div class="container">
+
+        <div class="row" data-aos="fade-up">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="gridarea__wraper">
+                    <div class="gridarea__img">
+                        <a href="/diploma-in-medical-laboratory-technology-dmlt"><img loading="lazy" src="{{asset('assets/img/service/facility-16.jpg')}}"
+                                alt="grid"></a>
+                        <div class="gridarea__small__button">
+                            <div class="grid__badge blue__color">Paramedical</div>
+                        </div>
+                        <div class="gridarea__small__icon">
+                            <a href="#"><i class="icofont-heart-alt"></i></a>
+                        </div>
+
+                    </div>
+                    <div class="gridarea__content">
+                        <div class="gridarea__list">
+                            <ul>
+                                <li>
+                                    <i class="icofont-chair"></i> 40 seats
+                                </li>
+                                <li>
+                                    <div class="text-end"><i class="icofont-clock-time"></i> 2 Years
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="gridarea__heading">
+                            <h3><a href="/diploma-in-medical-laboratory-technology-dmlt">Diploma in Medical Laboratory Technology
+                                    (DMLT)</a></h3>
+                        </div>
+                        <div class="text-justify">
+                            <p>DMLT is a two years course. A professional with DMLT examines and
+                                analyzes different body....</p>
+                        </div>
+                        <div class="gridarea__bottom">
+
+                            <a href="/diploma-in-medical-laboratory-technology-dmlt">
+                                <div class="gridarea__small__img">
+                                    <div class="gridarea__small__content ms-0">
+                                        <h6>Apply Now <i class="icofont-long-arrow-right"></i></h6>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="gridarea__wraper">
+                    <div class="gridarea__img">
+                        <a href="/diploma-in-medical-imaging-x-ray-technology-dmxt"><img loading="lazy" src="{{asset('assets/img/courses/dmxt.jpg')}}"
+                                alt="grid"></a>
+                        <div class="gridarea__small__button gridarea__small__button__1">
+                            <div class="grid__badge blue__color">Paramedical</div>
+                        </div>
+
+                    </div>
+                    <div class="gridarea__content">
+                        <div class="gridarea__list">
+                            <ul>
+                                <li>
+                                    <i class="icofont-chair"></i> 20 seats
+                                </li>
+                                <li>
+                                    <div class="text-end"><i class="icofont-clock-time"></i> 2 Years
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="gridarea__heading">
+                            <h3><a href="/diploma-in-medical-imaging-x-ray-technology-dmxt">Diploma in Medical Imaging/X-Ray
+                                    Technology (DMXT)</a></h3>
+                        </div>
+                        <div class="text-justify">
+                            <p>DMXT is an undergraduate radiology course and is all about how
+                                electromagnetic....</p>
+                        </div>
+                        <div class="gridarea__bottom">
+
+                            <a href="/diploma-in-medical-imaging-x-ray-technology-dmxt">
+                                <div class="gridarea__small__img">
+                                    <div class="gridarea__small__content ms-0">
+                                        <h6>Apply Now <i class="icofont-long-arrow-right"></i></h6>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="gridarea__wraper">
+                    <div class="gridarea__img">
+                        <a href="/diploma-in-medical-operation-technology-dmot"><img loading="lazy" src="{{asset('assets/img/courses/dmot.jpg')}}"
+                                alt="grid"></a>
+                        <div class="gridarea__small__button gridarea__small__button__1">
+                            <div class="grid__badge blue__color">Paramedical</div>
+                        </div>
+
+                    </div>
+                    <div class="gridarea__content">
+                        <div class="gridarea__list">
+                            <ul>
+                                <li>
+                                    <i class="icofont-chair"></i> 20 seats
+                                </li>
+                                <li>
+                                    <div class="text-end"><i class="icofont-clock-time"></i> 2 Years
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="gridarea__heading">
+                            <h3><a href="/diploma-in-medical-operation-technology-dmot">Diploma in Medical Operation Technology
+                                    (DMOT)</a></h3>
+                        </div>
+                        <div class="text-justify">
+                            <p>DMOT is a two-year diploma program designed to create trained and
+                                qualified professionals ....</p>
+                        </div>
+                        <div class="gridarea__bottom">
+
+                            <a href="/diploma-in-medical-operation-technology-dmot">
+                                <div class="gridarea__small__img">
+                                    <div class="gridarea__small__content ms-0">
+                                        <h6>Apply Now <i class="icofont-long-arrow-right"></i></h6>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                <div class="gridarea__wraper">
+                    <div class="gridarea__img">
+                        <a href="/diploma-in-medical-anesthesia-technology-dot-dat"><img loading="lazy" src="{{asset('assets/img/service/facility-9.jpg')}}"
+                                alt="grid"></a>
+                        <div class="gridarea__small__button gridarea__small__button__1">
+                            <div class="grid__badge blue__color">Paramedical</div>
+                        </div>
+
+                    </div>
+                    <div class="gridarea__content">
+                        <div class="gridarea__list">
+                            <ul>
+                                <li>
+                                    <i class="icofont-chair"></i> 20 seats
+                                </li>
+                                <li>
+                                    <div class="text-end"><i class="icofont-clock-time"></i> 2 Years
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="gridarea__heading">
+                            <h3><a href="/diploma-in-medical-anesthesia-technology-dot-dat">Diploma in Medical Anesthesia Technology
+                                    (DOT/DAT)</a></h3>
+                        </div>
+                        <div class="text-justify">
+                            <p>DOT/DAT leads the students to a comprehensive study of the field of....</p>
+                        </div>
+                        <div class="gridarea__bottom">
+
+                            <a href="/diploma-in-medical-anesthesia-technology-dot-dat">
+                                <div class="gridarea__small__img">
+                                    <div class="gridarea__small__content ms-0">
+                                        <h6>Apply Now <i class="icofont-long-arrow-right"></i></h6>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- .contact__section__end -->
+
+
+@endsection
---
+++ resources/WebView/Paramedicals/diploma-in-medical-operation-technology-dmot.blade.php
@@ -0,0 +1,217 @@
+@extends('WebView.WebViewLayouts.WebViewLayout')
+
+@section('webContent')
+    <!-- breadcrumbarea__section__start -->
+
+    <div class="breadcrumbarea" style="background-image: url('{{asset('assets/img/infobanner/infobanner-5.jpg')}}')">
+        <div class="dark-overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="breadcrumb__content__wraper" data-aos="fade-up">
+                        <div class="breadcrumb__title">
+                            <h2 class="heading text-white">Diploma in Medical Operation Technology (DMOT)</h2>
+                        </div>
+                        <div class="breadcrumb__inner">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li>Diploma in Medical Operation Technology (DMOT)</li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- breadcrumbarea__section__end-->
+
+    <!-- bsc-nursing__start -->
+    <div class="blogarea__2 sp_top_100 sp_bottom_100">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xl-8 col-lg-8">
+
+                    <div class="blogarae__img__2 course__details__img__2" data-aos="fade-up">
+                        <img loading="lazy" src="{{ asset('assets/img/courses/dmot.jpg') }}" alt="blog">
+                    </div>
+
+                    <div class="blog__details__content__wraper">
+                        <div class="course__button__wraper" data-aos="fade-up">
+                            <div class="course__button">
+                                <a href="javascript:void(0);"><i class="icofont-file-document"></i> Download
+                                    Prospectus</a>
+                            </div>
+                            <div class="course__date">
+                                <p>Last Update:<span> May 19, 2024</span></p>
+                            </div>
+                        </div>
+                        <div class="course__details__heading" data-aos="fade-up">
+                            <h3>About this course</h3>
+                        </div>
+                        <div class="course__details__paragraph text-justify" data-aos="fade-up">
+                            <p>DMOT is a two-year diploma program designed to create trained and qualified professionals who could assist medical professionals in the operation theatres of hospitals, nursing homes, intensive care units as well as emergency departments. The success of operation/surgical procedures and the safety of patients thus depend largely on the reliability of these operation theatre technicians.
+                            </p>
+                        </div>
+
+                        <h4 class="sidebar__title" data-aos="fade-up">Eligibility</h4>
+                        <p>Candidate has to be 10+2 Science from a recognized board
+                        </p>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="course__details__wraper" data-aos="fade-up">
+
+                                    <div class="d-flex align-items-center gap-3">
+                                        <div class="icon-badge">
+                                            <i class="icofont-check-circled"></i>
+                                        </div>
+                                        <div>
+                                            <h5 class="text-primary mb-0">Approvals</h5>
+                                            <p class="lh-1-5 mb-0">Indian Nursing Council ( INC ), Karnataka Nursing Council
+                                                ( KNC )</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <div class="course__details__wraper" data-aos="fade-up">
+
+                                    <div class="d-flex align-items-center gap-3">
+                                        <div class="icon-badge">
+                                            <i class="icofont-clock-time"></i>
+                                        </div>
+                                        <div>
+                                            <h5 class="text-primary mb-0">Duration</h5>
+                                            <p class="mb-0">2 Years</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-12">
+                                <div class="mb-3" data-aos="fade-up">
+                                    <a class="default__button me-2" target="_blank"
+                                        href="https://enlight.eduwego.in/online-admission/enlight-group-of-institutions">Apply
+                                        Now</a>
+                                    <a class="default__button" target="_blank"
+                                        href="https://enlight.eduwego.in/student-enquiry/enlight-group-of-institutions">Enquiry
+                                        Now</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="course__list__wraper" data-aos="fade-up">
+                            <div class="blog__details__heading__2">
+                                <h5>Documents Required For Admission</h5>
+                            </div>
+                            <div class="aboutarea__list__2 blog__details__list__2" data-aos="fade-up">
+                                <ul>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>10th / 12th Marksheet</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>10th / 12th Admit Card</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>Aadhhar Copy</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>Migration</p>
+                                    </li>
+                                    <li>
+                                        <i class="icofont-check"></i>
+                                        <p>12 Pic Colour Passport size Photo</p>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+                <div class="col-xl-4 col-lg-4">
+
+                    <div class="course__details__sidebar">
+
+                        <div class="blogsidebar__content__wraper__2" data-aos="fade-up">
+
+                            <h4 class="sidebar__title">Other Courses</h4>
+                            <ul class="course__details__populer__list">
+                                <li>
+
+                                    <div class="course__details__populer__img">
+                                        <img loading="lazy" src="{{ asset('assets/img/service/facility-16.jpg') }}"
+                                            alt="courses" width="90" height="70">
+                                    </div>
+                                    <div class="course__details__populer__content">
+                                        <h6>
+                                            <a href="/diploma-in-medical-laboratory-technology-dmlt">DMLT</a>
+                                        </h6>
+                                    </div>
+                                </li>
+                                <li>
+
+                                    <div class="course__details__populer__img">
+                                        <img loading="lazy" src="{{ asset('assets/img/service/facility-9.jpg') }}"
+                                            alt="courses" width="90" height="70">
+                                    </div>
+                                    <div class="course__details__populer__content">
+                                        <h6>
+                                            <a href="/diploma-in-medical-anesthesia-technology-dot-dat">DOT/DAT</a>
+                                        </h6>
+                                    </div>
+                                </li>
+                                <li>
+
+                                    <div class="course__details__populer__img">
+                                        <img loading="lazy" src="{{ asset('assets/img/courses/dmxt.jpg') }}"
+                                            alt="courses" width="90" height="70">
+                                    </div>
+                                    <div class="course__details__populer__content">
+                                        <h6>
+                                            <a href="/diploma-in-medical-imaging-x-ray-technology-dmxt">DMXT</a>
+                                        </h6>
+                                    </div>
+                                </li>
+                            </ul>
+
+                            <div class="course__summery__button">
+                                <p>Need any help in Admission ?</p>
+                                <a class="default__button default__button--3" href="tel:+91 9916073632"><i
+                                        class="icofont-phone"></i> +91 9916073632</a>
+                            </div>
+
+                        </div>
+                        <div class="blogsidebar__content__wraper__2" data-aos="fade-up">
+
+                            <h4 class="sidebar__title">Get in Touch</h4>
+                            <div class="get__touch__input">
+                                <input type="text" placeholder="Enter name*">
+                                <input type="text" placeholder="Enter your mail*">
+                                <input type="text" placeholder="Massage*">
+                                <a class="default__button" href="blog-details.html">Send Message</a>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bsc-nursing__end -->
+@endsection
