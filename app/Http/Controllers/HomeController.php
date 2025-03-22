6b357a2518fe99a3ce4d6917abf64c1661612969
chethan <chethanchethu99339@gmail.com>
2025-03-22 07:25:00 +0530

    Add HomeController to manage user authentication and dashboard view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $arrRes['userProfile'] = Auth::user();
+        // resources\views\ApplicationView\home.blade.php
+        return view('ApplicationView.home',$arrRes);
+    }
+}
